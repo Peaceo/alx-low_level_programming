30d75f95f6fd362b260863a25fe7dc3d15684eff
Peaceo <bunmipeace@yahoo.com>
2023-02-18 15:23:32 -0800

    compile a program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'This function prints a string to the user'
+ *Return: always 0 (success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
