ed90ff1cddd963cd495dee769420a5297fc0535b
Peaceo <bunmipeace@yahoo.com>
2023-02-18 15:04:24 -0800

    print string in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/*
+*The compiler starts compilation from the main keyword
+*This function prints a string to the use
+*Return "0"
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
