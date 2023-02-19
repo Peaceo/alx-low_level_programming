75b3dceb19a805be35fe22043c4ee31dbb0ee1d6
Peaceo <bunmipeace@yahoo.com>
2023-02-18 17:11:14 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
 
+#include <stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (0);
+fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+return (1);
 }
