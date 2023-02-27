6eaeb87285666c23d7aae9b34ac752aa917f1399
Peaceo <bunmipeace@yahoo.com>
2023-02-27 05:02:35 -0800

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,14 +6,12 @@
 */
 
 int main(void)
-
 {
+	int i;
 
-int i;
+	printf("Infinite loop incoming :(\n");
 
-printf("Infinite loop incoming :(\n");
-
-i = 0;
+	i = 0;
 
 /*while (i < 10)*/
 
@@ -23,8 +21,7 @@ i = 0;
 
 /*}*/
 
-printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-return (0);
-
+	return (0);
 }
